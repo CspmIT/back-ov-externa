70770552dc7112146e9e5d1a4fa89a39dd6ad655
agust1n <acomba@coopmorteros.coop>
2024-11-13 11:43:10 -0300

    fix: Se actualiza la conexion con oncativo para usar fortivpn

--- routes/Procoop.routes.js
+++ routes/Procoop.routes.js
@@ -1,8 +1,12 @@
 const expres = require('express')
 const { verifyToken } = require('../middleware/Auth.middleware')
-const { getRelationships } = require('../controllers/Procoop.controller')
+const {
+    getRelationships,
+    getAll,
+} = require('../controllers/Procoop.controller')
 const router = expres.Router()
 
 router.get('/getRelationships', verifyToken, getRelationships)
+router.get('/getAll', getAll)
 
 module.exports = router

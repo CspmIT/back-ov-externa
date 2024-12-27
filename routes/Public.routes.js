e12b1e90816545f2d7ff80dda9637af9d6a9632c
agust1n <acomba@coopmorteros.coop>
2024-12-27 09:45:31 -0300

    feat: se agregaron cambios para trabajar con imagenes

--- routes/Public.routes.js
+++ routes/Public.routes.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const os = require('os')
 const router = express.Router()
-const {
-    tokenVerify,
-    usersRegistered,
-} = require('../controllers/User.controller')
+const { tokenVerify } = require('../controllers/User.controller')
 // const { migrator1Up } = require('../controllers/migrations.controller')
 const {
     login,
@@ -26,29 +23,11 @@ const {
     migrationCity,
     migrationState,
     testConectOncativo,
-    oncativoUser,
 } = require('../controllers/Procoop.controller')
-const {
-    Commentaries,
-    addCommentary,
-    Popups,
-    addPopup,
-    addInformation,
-    Informations,
-    addImageInformation,
-    ImageInformations,
-} = require('../controllers/Managment.controller')
-const {
-    relationUserCooptech,
-    loginCooptech,
-    tokenCooptech,
-} = require('../controllers/Cooptech.controller')
+
 const { paymentMercadoPago } = require('../controllers/Payment.controller')
 
 // RUTAS PARA AUTH
-
-router.post('/generateTokenCooptech', tokenCooptech)
-router.post('/loginCooptech', loginCooptech)
 router.post('/login', login)
 router.post('/register', register)
 router.post('/changePassword', changePassword)
@@ -58,35 +37,18 @@ router.post('/password_recover', password_recover)
 router.post('/existToken', tokenVerify)
 
 // RUTAS PARA COOPTECH
-router.post('/relationUserCooptech', relationUserCooptech)
 
 router.get('/status', testConectOncativo)
-// router.get('/users', migrationUser)
-// router.get('/email', sendEmail)
-
-//RUTAS DE SERVICIOS
-// router.get('/getConsumo', customerConsumption)
 
 //RUTAS DE PAGOS
 router.get('/facturas', getInvoice)
 router.get('/existinvoice', existInvoice)
-// router.get('/pruebaMigration', migrator1Up)
-// router.get('/MigrationLocation', migrationCity)
 
 router.get('/testConect', testConect)
 router.post('/searchDni', searchByDNI)
 router.post('/searchCuit', searchByCuit)
 
 //RUTAS INTERNAS
-router.get('/Commentaries', Commentaries)
-router.get('/getPopups', Popups)
-router.post('/addPopup', addPopup)
-router.post('/addInformation', addInformation)
-router.get('/informations', Informations)
-router.post('/addImageInformation', addImageInformation)
-router.get('/imageInformations', ImageInformations)
-router.get('/getUsersRegistered', usersRegistered)
-
 router.get('/mercadopago', paymentMercadoPago)
 
 router.get('/getIp', (req, res) => {
